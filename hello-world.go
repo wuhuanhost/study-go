c86dde62d4c6139a53cb825a3588d13fcbf3f967
wuhuanhost <post.thinking@gmail.com>
2016-12-23 13:20:44 +0800

    添加了json解析的包

--- hello-world.go
+++ hello-world.go
@@ -1,7 +1,13 @@
 package main
-import 'fmt'
-
+import "fmt"
+/*json解析的包*/
+import "github.com/json-iterator/go"
 func main(){
+  var str="我的第一个go语言代码";
   fmt.Printf("hello world!!!\n");
-  fmt.Printf("我的第一个go语言代码");
+  fmt.Printf(str+"\n");
+  iter := jsoniter.ParseString(`[0,1,2,3]`)
+  val := []int{}
+  iter.Read(&val)
+  fmt.Println(val[3])
 }
